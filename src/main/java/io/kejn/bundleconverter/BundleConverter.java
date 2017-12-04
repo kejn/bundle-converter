b5d514e966e0a220dac6ef7c4d8334399dd038d7
kejn <kaen04@gmail.com>
2017-12-05 00:24:40 +0100

    Added cell styling. TODO: tests.

--- src/main/java/io/kejn/bundleconverter/BundleConverter.java
+++ src/main/java/io/kejn/bundleconverter/BundleConverter.java
@@ -1,17 +1,22 @@
 package io.kejn.bundleconverter;
 
-import java.util.Properties;
-import java.util.Set;
+import java.util.function.Function;
 
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.VerticalAlignment;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class BundleConverter {
 
-
+    private static final short FONT_SIZE = 10;
+    private static final String FONT_NAME = "Arial";
+    
     public static final int HEADER_ROW = 0;
     private static final int CONTENT_FIRST_ROW = 1;
 
@@ -30,52 +35,75 @@ public class BundleConverter {
 	return workbook;
     }
 
+    private void createHeader(Sheet sheet, BundleGroup group) {
+	CellStyle cellStyle = getHeaderCellStyle(sheet.getWorkbook());
+	String defaultValue = Language.DEFAULT.getDisplayLanguage();
+
+	createRow(group, sheet, HEADER_ROW, cellStyle, KEY_LABEL, defaultValue, language -> {
+	    return language.getDisplayLanguage();
+	});
+    }
+
     private void createTranslations(Sheet sheet, BundleGroup group) {
-	Set<String> keys = group.stringPropertyNames();
+	CellStyle cellStyle = getContentCellStyle(sheet.getWorkbook());
 
 	int rowIndex = CONTENT_FIRST_ROW;
-	for (String key : keys) {
-	    Row row = sheet.createRow(rowIndex);
-
-	    Cell cell = row.createCell(KEY_COLUMN);
-	    cell.setCellValue(key);
-
-	    cell = row.createCell(DEFAULT_COLUMN);
-	    cell.setCellValue(group.getProperty(key, Language.DEFAULT));
-
-	    int colIndex = DEFAULT_COLUMN + 1;
-	    for (Language language : group.supportedLanguages()) {
-		if (Language.DEFAULT.equals(language)) {
-		    continue;
-		}
-		cell = row.createCell(colIndex);
-		cell.setCellValue(group.getProperty(key, language));
-
-		++colIndex;
-	    }
+	for (String key : group.stringPropertyNames()) {
+	    String defaultValue = group.getProperty(key, Language.DEFAULT);
+	    createRow(group, sheet, rowIndex, cellStyle, key, defaultValue, language -> {
+		return group.getProperty(key, language);
+	    });
 	    ++rowIndex;
 	}
     }
 
-    private void createHeader(Sheet sheet, BundleGroup group) {
-	Row row = sheet.createRow(HEADER_ROW);
+    private void createRow(BundleGroup group, Sheet sheet, int rowIndex, CellStyle cellStyle, String key,
+	    String defaultValue, Function<Language, String> valueConverter) {
+	Row row = sheet.createRow(rowIndex);
 
-	Cell cell = row.createCell(KEY_COLUMN);
-	cell.setCellValue(KEY_LABEL);
-
-	cell = row.createCell(DEFAULT_COLUMN);
-	cell.setCellValue(Language.DEFAULT.getDisplayLanguage());
+	createCellWithStyle(row, KEY_COLUMN, cellStyle, key);
+	createCellWithStyle(row, DEFAULT_COLUMN, cellStyle, defaultValue);
 
 	int colIndex = DEFAULT_COLUMN + 1;
 	for (Language language : group.supportedLanguages()) {
 	    if (Language.DEFAULT.equals(language)) {
 		continue;
 	    }
-	    cell = row.createCell(colIndex);
-	    cell.setCellValue(language.getDisplayLanguage());
-
+	    createCellWithStyle(row, colIndex, cellStyle, valueConverter.apply(language));
 	    ++colIndex;
 	}
+    }
+    
+    private void createCellWithStyle(Row row, int cellIndex, CellStyle cellStyle, String value) {
+	Cell cell = row.createCell(cellIndex);
+	cell.setCellStyle(cellStyle);
+	cell.setCellValue(value);
+	cell.getStringCellValue();
+    }
+
+    private CellStyle getHeaderCellStyle(Workbook workbook) {
+	Font font = createFont(workbook);
+	font.setBold(true);
+	
+	CellStyle cellStyle = getContentCellStyle(workbook);
+	cellStyle.setFont(font);
+	return cellStyle;
+    }
+
+    private CellStyle getContentCellStyle(Workbook workbook) {
+	CellStyle cellStyle = workbook.createCellStyle();
+	cellStyle.setFont(createFont(workbook));
+	cellStyle.setWrapText(true);
+	cellStyle.setAlignment(HorizontalAlignment.LEFT);
+	cellStyle.setVerticalAlignment(VerticalAlignment.TOP);
+	return cellStyle;
+    }
+
+    private Font createFont(Workbook workbook) {
+	Font font = workbook.createFont();
+	font.setFontName(FONT_NAME);
+	font.setFontHeightInPoints(FONT_SIZE);
+	return font;
     }
 
 }
